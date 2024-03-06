a260ba5f103f121fabf387402a5a01c2fec3a0c8
sr28_04 <snehilrai678.sr@gmail.com>
2024-03-06 17:29:52 +0530

    changes in js

--- index.js
+++ index.js
@@ -13,27 +13,27 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
-addButtonEl.addEventListener("click", function() {
+addButtonEl.addEventListener("click", function () {
     let inputValue = inputFieldEl.value
-    
+
     push(shoppingListInDB, inputValue)
-    
+
     clearInputFieldEl()
 })
 
-onValue(shoppingListInDB, function(snapshot) {
+onValue(shoppingListInDB, function (snapshot) {
     if (snapshot.exists()) {
         let itemsArray = Object.entries(snapshot.val())
-    
+
         clearShoppingListEl()
-        
+
         for (let i = 0; i < itemsArray.length; i++) {
             let currentItem = itemsArray[i]
             let currentItemID = currentItem[0]
             let currentItemValue = currentItem[1]
-            
+
             appendItemToShoppingListEl(currentItem)
-        }    
+        }
     } else {
         shoppingListEl.innerHTML = "No items here... yet"
     }
@@ -50,16 +50,16 @@ function clearInputFieldEl() {
 function appendItemToShoppingListEl(item) {
     let itemID = item[0]
     let itemValue = item[1]
-    
+
     let newEl = document.createElement("li")
-    
+
     newEl.textContent = itemValue
-    
-    newEl.addEventListener("click", function() {
+
+    newEl.addEventListener("dblclick", function () {
         let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
-        
+
         remove(exactLocationOfItemInDB)
     })
-    
+
     shoppingListEl.append(newEl)
 }
